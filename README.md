a9ed4bcf83226773cc1b9259ef135a100d2be9cb
kic <ch9.ki7@gmail.com>
2013-08-21 15:52:31 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 JavaRhinoEnvjsD3
 ================
 
-Render SVG in a Headless Java Environment with Rhino + Envjs + D3
+Render SVG in a Headless Java Environment with Rhino + Envjs + D3.
+
+I ever wanted to know if it is possible to do so. And yes, here is the proof. You can render SVG in a complete headless java environment.
+
+The man task is to geht your rhino and evjs setup, see my post on stackoverflow: http://stackoverflow.com/a/18016155/1298461
+
+All the next steps are pretty forward, simply compile this java source and see.
+
+Sadly JavaFX webengine is not running on a complete headless server, hopefully it is with JavaFX 8. So I hope this may help someone!
+
+Cheers 
+KIC
+
+
+
---
+++ src/kic/engine/plot/svg/render/Renderer.java
@@ -0,0 +1,88 @@
+package kic.engine.plot.svg.render;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+/**
+ *
+ * @author Kic
+ *   later (javafx8) headless javafx 
+ *      with Xvfb http://docs.codehaus.org/display/FEST/Running+FEST+under+Xvfb
+ *      and fake jfxpanel https://javafx-jira.kenai.com/browse/RT-32335?page=com.atlassian.jira.plugin.system.issuetabpanels:all-tabpanel
+ *
+ *   share objects http://stackoverflow.com/questions/16633660/passing-custom-java-objects-to-webengine-executescript    
+ */
+
+public class Renderer {
+    private final Context cx = ContextFactory.getGlobal().enterContext();
+    private Scriptable scope = cx.initStandardObjects();
+    
+    public static void main(String[] args) throws Exception {
+        new Renderer().test();
+    }
+    
+    public Renderer() {
+        // load missing functions 
+        loadScript("js/missingfunctions.js");
+        
+        // load envjs core
+        loadScript("js/envjs/dist12/core.js");
+        loadScript("js/envjs/dist12/rhino.js");
+        
+        // load envjs dom.js |→event.js |→html.js |→timer.js |→parser.js |→xhr.js |→window.js 
+        loadScript("js/envjs/dist12/console.js");
+        loadScript("js/envjs/dist12/dom.js");
+        loadScript("js/envjs/dist12/event.js");
+        loadScript("js/envjs/dist12/html.jsr");
+        loadScript("js/envjs/dist12/css.js");
+        loadScript("js/envjs/dist12/parser.js");
+        loadScript("js/envjs/dist12/xhr.js");
+        loadScript("js/envjs/dist12/timer.js");
+        loadScript("js/envjs/dist12/window.js");
+        
+        // load css selector fix
+        loadScript("js/envjs/dist12/sizzle.js");
+        
+        // load d3
+        //loadScript("js/d3/d3.v3.min.js"); // better to load
+        loadScript("js/d3/d3.v3.js"); // better to debug
+    }
+    
+    public final void loadScript(String name) {
+        InputStreamReader readerJQ = null;
+        try {
+            InputStream resourceAsStream = Renderer.class.getResourceAsStream(name);
+            readerJQ = new InputStreamReader(resourceAsStream);
+            cx.evaluateReader(scope, readerJQ, name, 1, null);
+            readerJQ.close();
+        } catch (IOException e) {               
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public final void setObject(String name, Object o) {
+        ScriptableObject.putProperty(scope, name, Context.javaToJS(o, scope));
+    }
+    
+    public String getBody() {
+        cx.evaluateString(scope, "var svgSource = document.getElementsByTagName('body')[0].innerHTML;", "getsvg", 1, null);
+        return scope.get("svgSource", scope).toString();
+    }
+    
+    public void eval(String script) {
+        cx.evaluateString(scope, script, ""+script.hashCode(), 1, null);
+    }
+    
+    public void test() {
+        setObject("test", new java.util.Date());
+        eval("d3.select('body').selectAll('p').data([1,2,3]).enter().append('p').text('hello');"
+           + "print(document.innerHTML);");
+        
+        System.out.println(getBody());
+    }
+}
